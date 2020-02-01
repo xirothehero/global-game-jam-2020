0dd82dc43552f0787fba4da78468da2abb6e60ef
Ali Kedwaii <ali.kedwaii@utexas.edu>
2020-02-01 15:55:03 -0600

    Segment + some management

---
+++ RoastedToasted/Assets/Scripts/Segment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Segment : MonoBehaviour {
+
+    [SerializeField] private Transform _spawnPoint;
+
+    public Transform SpawnPoint => _spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.tag.Equals("Player"))
+        {
+            GameManager.instance.IncrementSegment();
+        }
+    }
+}
