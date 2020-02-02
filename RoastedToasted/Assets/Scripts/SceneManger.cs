179b6b843238a56b4cfbe65f324eab1f2d4644bd
justin2jam <justin2jam@gmail.com>
2020-02-02 13:37:36 -0600

    titled Screen

--- RoastedToasted/Assets/Scripts/SceneManger.cs
+++ RoastedToasted/Assets/Scripts/SceneManger.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public void LoadScene (int level) {
+        SceneManager.LoadScene(level);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void QuitGame () {
+        Application.Quit();
     }
 }
