56efb3c0ea6173ac0164366128d335b322b316f7
justin2jam <justin2jam@gmail.com>
2020-02-02 05:05:48 -0600

    game level

--- RoastedToasted/Assets/Scripts/NextSegment.cs
+++ RoastedToasted/Assets/Scripts/NextSegment.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class NextSegment : MonoBehaviour
 {
+    bool entered = false;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag.Equals("Player"))
         {
-            GameManager.instance.IncrementSegment();
+            if (!entered)
+            {
+                GameManager.instance.IncrementSegment();
+                entered = true;
+            }
         }
     }
 }
--- RoastedToasted/Assets/Scripts/SegmentComplete.cs
+++ RoastedToasted/Assets/Scripts/SegmentComplete.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SegmentComplete : MonoBehaviour
 {
     public GameObject[] platforms;
+    public GameObject tear;
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             foreach(GameObject platform in platforms) {
                 platform.GetComponent<Platform>().complete();
+                tear.GetComponent<Animator>().SetTrigger("fade");
             }
         }
     }
