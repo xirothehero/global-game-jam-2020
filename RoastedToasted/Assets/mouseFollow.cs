7f54912c5598368c22ef7cc582af81aa5ac53dd2
justin2jam <justin2jam@gmail.com>
2020-02-02 12:28:09 -0600

    frend works

--- RoastedToasted/Assets/mouseFollow.cs
+++ RoastedToasted/Assets/mouseFollow.cs
@@ -4,33 +4,65 @@ using UnityEngine;
 
 public class mouseFollow : MonoBehaviour
 {
-    public GameObject point;
     public bool pathReached = false;
-    // Start is called before the first frame update
+    public float maxSpeed = 10;
+    public float smoothTime = 0.1f;
+
+    Vector3 mousePos;
+
+    Vector3 velocity = Vector3.zero;
+    Rigidbody2D rb;
+    Vector3 prevLoc = Vector3.zero;
+    Vector3 currPos;
+    Vector3 direction;
+    bool right = false;
     void Start()
     {
-        StartCoroutine(MoveToMouse());
+        rb = GetComponent<Rigidbody2D>();
 
-        //StartCoroutine(coroutineA());
+    //     StartCoroutine(MoveToMouse());
+
+    //     StartCoroutine(coroutineA());
     }
 
     // Update is called once per frame
     void Update()
     {
+        currPos = transform.position;
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector3 newPos = Vector3.SmoothDamp(currPos, mousePos, ref velocity, smoothTime, maxSpeed);
+        newPos.z = currPos.z;
+        transform.position = newPos;
+
         
+        direction = (transform.position - prevLoc) / Time.deltaTime;
+        
+        if (direction.x > 0.01 && !right) {
+            GetComponent<SpriteRenderer>().flipX = true;
+            print("right");
+            right = true;
+
+        } else if ( direction.x < -0.01 && right) {
+            GetComponent<SpriteRenderer>().flipX = false;
+            right = false;
+        }
+        prevLoc = transform.position;
     }
 
-    IEnumerator MoveToMouse() {
-        Vector3 mousePos = point.transform.position;//Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-        yield return null;
-        while(true) {
-            if (Vector3.Distance(transform.position, mousePos) < 0.01) {
-                pathReached = true;
-            }
-        }
-    }
+
+    // IEnumerator MoveToMouse() {
+    //     Vector3 mousePos = point.transform.position;//Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+    //     yield return null;
+    //     while(true) {
+    //         if (Vector3.Distance(transform.position, mousePos) < 0.01) {
+    //             pathReached = true;
+    //         }
+    //     }
+    // }
 
 //  yield return new WaitForSeconds(Random.Range(0.0f, 2f));
+
+    /*
     IEnumerator coroutineA()
     {
         // wait for 1 second
@@ -45,5 +77,5 @@ public class mouseFollow : MonoBehaviour
         Debug.Log("coroutineB created");
         yield return new WaitForSeconds(2.5f);
         Debug.Log("coroutineB enables coroutineA to run");
-    }
+    } */
 }
