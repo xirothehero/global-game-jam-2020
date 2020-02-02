ac7b984714064cf58797069f7d642ce82fab8dee
justin2jam <justin2jam@gmail.com>
2020-02-02 14:47:24 -0600

    cutscenes; scene changes

---
+++ RoastedToasted/Assets/cutscene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class cutscene : MonoBehaviour
+{
+    Animator anim;
+    int scene = 0;
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            scene += 1;
+            anim.SetInteger("Scene", scene);
+            if (scene >= 4) {
+                SceneManager.LoadScene(2);
+            }
+        }
+        
+    }
+}
